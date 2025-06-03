5518b74b51fb299a4a956fc95d9ed20831e7891c
khumaidi kharis az-zacky <zackyskak5@gmail.com>
2025-06-03 23:41:29 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -1,39 +1,56 @@
 #include "std_lib.h"
 
-int div(int a, int b)
-{
-  //TODO: Implementasi pembagian
-  //NOTE: HARUS BISA ANGKA NEGATIF
+int div(int a, int b) {
+  int neg = 0, res = 0;
+  if (a < 0) { a = -a; neg = !neg; }
+  if (b < 0) { b = -b; neg = !neg; }
+  while (a >= b) { a -= b; res++; }
+  return neg ? -res : res;
 }
 
-int mod(int a, int b)
-{
-  //TODO: Implementasi modulus
+int mod(int a, int b) {
+  int neg = a < 0;
+  if (a < 0) a = -a;
+  if (b < 0) b = -b;
+  while (a >= b) a -= b;
+  return neg ? -a : a;
 }
 
-bool strcmp(char *str1, char *str2)
-{
-  //TODO: Implementasi perbandingan string
+bool strcmp(char *s1, char *s2) {
+  while (*s1 && *s2) {
+    if (*s1 != *s2) return false;
+    s1++; s2++;
+  }
+  return *s1 == *s2;
 }
 
-void strcpy(char *dst, char *src)
-{
-  //TODO: Implementasi penyalinan string
+void strcpy(char *dst, char *src) {
+  while (*src) *dst++ = *src++;
+  *dst = 0;
 }
 
-void clear(byte *buf, unsigned int size)
-{
-  //TODO: Implementasi pembersihan buffer
+void clear(byte *buf, unsigned int size) {
+  for (int i = 0; i < size; i++) buf[i] = 0;
 }
 
-void atoi(char *str, int *num)
-{
-  //TODO: Implementasi konversi string ke integer
-  //NOTE: HARUS BISA ANGKA NEGATIF
+void atoi(char *str, int *num) {
+  int sign = 1;
+  *num = 0;
+  if (*str == '-') { sign = -1; str++; }
+  while (*str) *num = *num * 10 + (*str++ - '0');
+  *num *= sign;
 }
 
-void itoa(int num, char *str)
-{
-  //TODO: Implementasi konversi integer ke string
-  //NOTE: HARUS BISA ANGKA NEGATIF
+void itoa(int num, char *str) {
+  int i = 0, neg = 0;
+  if (num < 0) { neg = 1; num = -num; }
+  do {
+    str[i++] = num % 10 + '0';
+    num /= 10;
+  } while (num);
+  if (neg) str[i++] = '-';
+  str[i] = 0;
+  for (int j = 0; j < i / 2; j++) {
+    char t = str[j]; str[j] = str[i - j - 1]; str[i - j - 1] = t;
+  }
 }
