776e0cf8077fa683021ccb1e4b16bf25b1124d54
khumaidi kharis az-zacky <zackyskak5@gmail.com>
2025-06-03 23:42:16 +0700

    Update kernel.h

--- include/kernel.h
+++ include/kernel.h
@@ -2,7 +2,6 @@
 #define __KERNEL_H__
 
 #include "std_type.h"
-
 extern void putInMemory(int segment, int address, char character);
 extern int interrupt(int number, int AX, int BX, int CX, int DX);
 unsigned int getBiosTick();
@@ -10,5 +9,6 @@ unsigned int getBiosTick();
 void printString(char* str);
 void readString(char* buf);
 void clearScreen();
+extern int textColor;
 
-#endif // __KERNEL_H__
+#endif
